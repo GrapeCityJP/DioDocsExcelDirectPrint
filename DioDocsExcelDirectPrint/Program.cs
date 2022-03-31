03c3c0e685fc5496a4b7a6a9797d5498aa99afcb
Kuni <kuni.ujiie@grapecity.com>
2022-03-31 15:08:45 +0900

    プロジェクト ファイルを追加します。

---
+++ DioDocsExcelDirectPrint/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using GrapeCity.Documents.Excel;
+
+Console.WriteLine("Excelファイルをプリンタダイアログ表示なしで直接印刷します");
+
+Workbook.SetLicenseKey("");
+
+// Excelファイルを読み込み
+var workbook = new Workbook();
+workbook.Open(@"InvoiceJP.xlsx");
+
+// 印刷オプションを作成
+PrintOutOptions options = new PrintOutOptions();
+
+// 印刷するプリンターを設定
+options.ActivePrinter = "Microsoft Print to PDF";
+
+// 3部印刷 
+options.Copies = 3;
+
+// このワークブックを「Microsoft Print to PDF」で印刷
+workbook.PrintOut(options);
